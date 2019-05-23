a2a053865e04f2171ca1a4035505b11597c50d86
Coo van Leeuwen <info@cerem.co>
2019-05-23 11:51:03 +0700

    some simple changes

--- src/pipes/image/localImage.ts
+++ src/pipes/image/localImage.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Platform } from 'ionic-angular';
 
 @Pipe({
   name: 'localImage',
@@ -6,7 +7,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class LocalImagePipe implements PipeTransform {
   private win: any = window;
 
+  constructor(private platform: Platform) {
+
+  }
+
   transform(value: string) {
+    if (!this.platform.is('cordova')) {
+      return value;
+    }
     return this.win.Ionic.WebView.convertFileSrc(value);
   }
 }
